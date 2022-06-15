2127a2c52e94ef3be80fbc753487860e797074b8
Edinson <edinsonmadrid@gmail.com>
2022-06-15 02:55:38 -0300

    update V2

--- client/src/Components/Pages/Card/Card.jsx
+++ client/src/Components/Pages/Card/Card.jsx
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { searchTypes } from "../../Actions";
+
 
 const Card = () => {   
+    const dispatch = useDispatch();
+    const allRecipes = useSelector((state) => state.recipes);
+
+    useEffect(() => {
+        dispatch(searchTypes());
+    }, []);
 
     return (
-        <h1>Estoy en Card</h1>
+        <>
+            
+            <h1>Estoy en Card</h1>
+        </>
     )
 }
 
