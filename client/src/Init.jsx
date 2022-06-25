e598e0d5ade232b74138d1f313c1884b1916e853
Edinson <edinsonmadrid@gmail.com>
2022-06-25 13:15:42 -0300

    change init.jsx

--- client/src/Init.jsx
+++ client/src/Init.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route, BrowserRouter, Switch } from "react-router-dom";
-import { LandingPage } from "./Components/LandingPage";
 import {Home} from "./Components/Home";
 import {About} from "./Components/About.jsx";
 import {RecipeCreate} from "./Components/RecipeCreate.jsx";
@@ -15,13 +14,11 @@ function Init() {
       <div >
         <Navbar />
         <Switch >
-          {/* <Route exact path="/" component={LandingPage} /> */}
           <Route path="/home" component={Home}  />
           <Route path="/about" component={About} />
           <Route path="/create" component={RecipeCreate} />
           <Route exact path="/recipes"  component={Card} />
           <Route path="/recipes/:id" component={DetailRecipe}  />
-          {/* <Route path="/home/:id" component={DetailRecipe} /> */}
         </Switch>
       </div>
     </BrowserRouter>
