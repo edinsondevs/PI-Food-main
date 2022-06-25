6c589587dca0872c6dd5e84c14f5c0536caa4512
Edinson <edinsonmadrid@gmail.com>
2022-06-25 13:14:54 -0300

    change app - create  init.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,16 @@
 import { Routes, Route, BrowserRouter, Switch } from "react-router-dom";
 import { LandingPage } from "./Components/LandingPage";
-import {Home} from "./Components/Home";
-import {About} from "./Components/About.jsx";
-import {RecipeCreate} from "./Components/RecipeCreate.jsx";
-import { Navbar } from './Components/Navbar.jsx';
 import './App.css'; 
-import { Card } from './Components/Card';
-import DetailRecipe  from './Components/DetailRecipe.jsx';
+import Init from './Init';
 
 
 function App() {
   return (
     <BrowserRouter>
       <div >
-        <Navbar />
         <Switch >
           <Route exact path="/" component={LandingPage} />
-          <Route path="/home" component={Home}  />
-          <Route path="/about" component={About} />
-          <Route path="/create" component={RecipeCreate} />
-          <Route exact path="/recipes"  component={Card} />
-          <Route path="/recipes/:id" component={DetailRecipe}  />
-          {/* <Route path="/home/:id" component={DetailRecipe} /> */}
+          <Route path="/*" component={Init}  />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ client/src/Init.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route, BrowserRouter, Switch } from "react-router-dom";
+import { LandingPage } from "./Components/LandingPage";
+import {Home} from "./Components/Home";
+import {About} from "./Components/About.jsx";
+import {RecipeCreate} from "./Components/RecipeCreate.jsx";
+import { Navbar } from './Components/Navbar.jsx';
+import './App.css'; 
+import { Card } from './Components/Card';
+import DetailRecipe  from './Components/DetailRecipe.jsx';
+
+
+function Init() {
+  return (
+    <BrowserRouter>
+      <div >
+        <Navbar />
+        <Switch >
+          {/* <Route exact path="/" component={LandingPage} /> */}
+          <Route path="/home" component={Home}  />
+          <Route path="/about" component={About} />
+          <Route path="/create" component={RecipeCreate} />
+          <Route exact path="/recipes"  component={Card} />
+          <Route path="/recipes/:id" component={DetailRecipe}  />
+          {/* <Route path="/home/:id" component={DetailRecipe} /> */}
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default Init;
