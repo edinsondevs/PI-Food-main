364287e9a9f48043edcf3fdb26a9f5ea114a1ad1
Edinson <edinsonmadrid@gmail.com>
2022-09-26 13:20:24 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -8,13 +8,20 @@ require('./db.js');
 
 const server = express();
 
-server.name = 'API';
+const whiteList = [
+  'http://localhost:3000',
+  'http://localhost:3001'
+]
 
+
+server.name = 'API';
+server.use(cors({ origin: whiteList }));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
+server.use((req, res, next) =>
+{
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -25,7 +32,8 @@ server.use((req, res, next) => {
 server.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) =>
+{ // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
